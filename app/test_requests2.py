c27c05ccfb84b8602db29d0d8371757cde0e5bed
Татьяна Варакина <tchursanova9@gmail.com>
2026-02-11 10:36:52 +0300

    Remove test_requests2.py

--- app/test_requests2.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-response=get('https://images-assets.nasa.gov/image/ACD26-0007-049/ACD26-0007-049~small.jpg')
-
-print(response)
-print(response.status_code)
-print(response.headers.get("Content-Type"))
-
-with open('test.jpg', 'wb') as file:
-    file.write(response.content)
